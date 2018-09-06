a03623288847b21e89f3e1d0ed558e86908f87c3
Krzysztof Sofroniuk <Krzysztof.Sofroniuk@jeppesen.com>
2018-09-06 15:13:26 +0200

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,55 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# sudo apt-get update 
+# sudo apt-get install default-jdk
+package 'default-jdk'
+
+# sudo groupadd tomcat
+group 'tomcat'
+
+# sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+user 'tomcat' do
+  manage_home false
+  shell '/bin/false'
+  group 'tomcat'
+  home '/opt/tomcat'
+end
+
+# wget http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.33/bin/apache-tomcat-8.5.33.tar.gz
+remote_file 'apache-tomcat-8.5.33.tar.gz' do
+  source 'http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.33/bin/apache-tomcat-8.5.33.tar.gz'
+end
+
+directory '/opt/tomcat' do
+  # action :create
+end
+
+# TODO: NOT DESIRED STATE
+execute 'tar xzvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+
+# TODO: NOT DESIRED STATE
+execute 'chgrp -R tomcat /opt/tomcat'
+execute 'chmod -R g+r /opt/tomcat/conf'
+
+directory '/opt/tomcat/conf' do
+  mode '0010'
+end
+
+# TODO: NOT DESIRED STATE
+execute 'chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/'
+
+template '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service.erb'
+end
+
+# TODO: NOT DESIRED STATE
+execute 'systemctl daemon-reload'
+
+service 'tomcat' do
+  action [:start, :enable]
+end
+
+# TODO: NOT DESIRED STATE
+execute 'ufw allow 8080'
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -16,3 +16,38 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+
+describe command('curl http://localhost:8080') do
+  its(:stdout) { should match /Tomcat/ }
+end
+
+describe package('default-jdk') do
+  it { should be_installed }
+end
+
+describe group('tomcat') do
+  it { should exist }
+end
+
+describe user('tomcat') do
+  it { should exist }
+  its('group') { should eq 'tomcat' }
+  it { should have_home_directory '/opt/tomcat' }
+end
+
+describe file ('/opt/tomcat') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file ('/opt/tomcat/conf') do
+  it { should exist }
+  it { should be_mode 0010 }
+end
+
+%w[ webapps work temp logs ].each do |path|
+  describe file ("/opt/tomcat/#{path}") do
+    it { should exist }
+    it { should be_owned_by 'tomcat' }
+  end
+end
